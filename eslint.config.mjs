b9a8003305c287a2d0fed21145702b55aaec4567
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-17 21:54:31 +0700

    init lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import daStyle from "eslint-config-dicodingacademy";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  daStyle,
 ];

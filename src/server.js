be08ee3442e77972cac598a46364a5a7153293c4
Yasir <yasiramrr@gmail.com>
2025-02-20 09:56:06 +0700

    add

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
-const routes = require('./api/notes/routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesServices');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -13,7 +15,12 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);

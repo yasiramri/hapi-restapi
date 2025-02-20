2310a5ea19af85af4d925d5f7c0ed86f7eecca35
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-20 08:15:53 +0700

    hapi rest

--- src/handler.js
+++ src/handler.js
@@ -1,15 +1,20 @@
-const { nanoid } = require('nanoid');
-const notes = require('./');
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
 
 const addNoteHandler = (request, h) => {
-  const { title = 'untitled', tags, body } = request.payload;
+  const { title = "untitled", tags, body } = request.payload;
 
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
 
   const newNote = {
-    title, tags, body, id, createdAt, updatedAt,
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
   };
 
   notes.push(newNote);
@@ -18,8 +23,8 @@ const addNoteHandler = (request, h) => {
 
   if (isSuccess) {
     const response = h.response({
-      status: 'success',
-      message: 'Catatan berhasil ditambahkan',
+      status: "success",
+      message: "Catatan berhasil ditambahkan",
       data: {
         noteId: id,
       },
@@ -29,15 +34,15 @@ const addNoteHandler = (request, h) => {
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Catatan gagal ditambahkan',
+    status: "fail",
+    message: "Catatan gagal ditambahkan",
   });
   response.code(500);
   return response;
 };
 
 const getAllNotesHandler = () => ({
-  status: 'success',
+  status: "success",
   data: {
     notes,
   },
@@ -50,7 +55,7 @@ const getNoteByIdHandler = (request, h) => {
 
   if (note !== undefined) {
     return {
-      status: 'success',
+      status: "success",
       data: {
         note,
       },
@@ -58,8 +63,8 @@ const getNoteByIdHandler = (request, h) => {
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Catatan tidak ditemukan',
+    status: "fail",
+    message: "Catatan tidak ditemukan",
   });
   response.code(404);
   return response;
@@ -83,16 +88,16 @@ const editNoteByIdHandler = (request, h) => {
     };
 
     const response = h.response({
-      status: 'success',
-      message: 'Catatan berhasil diperbarui',
+      status: "success",
+      message: "Catatan berhasil diperbarui",
     });
     response.code(200);
     return response;
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    status: "fail",
+    message: "Gagal memperbarui catatan. Id tidak ditemukan",
   });
   response.code(404);
   return response;
@@ -106,16 +111,16 @@ const deleteNoteByIdHandler = (request, h) => {
   if (index !== -1) {
     notes.splice(index, 1);
     const response = h.response({
-      status: 'success',
-      message: 'Catatan berhasil dihapus',
+      status: "success",
+      message: "Catatan berhasil dihapus",
     });
     response.code(200);
     return response;
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    status: "fail",
+    message: "Catatan gagal dihapus. Id tidak ditemukan",
   });
   response.code(404);
   return response;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const Hapi = require("@hapi/hapi");
-const routes = require("./api/notes/routes");
+const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 3000,
     host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {

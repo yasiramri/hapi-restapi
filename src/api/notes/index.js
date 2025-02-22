586b1bb57333eb34de33ded1c2edeba4edb07cfc
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-22 17:38:57 +0700

    add error custom" git commi -m "add custom error"

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -4,9 +4,9 @@ const routes = require('./routes');
 
 module.exports = {
   name: 'notes',
-  version: '1.00',
-  register: async (server, { service }) => {
-    const notesHandler = new NotesHandler(service);
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesHandler(service, validator);
     server.route(routes(notesHandler));
   },
 };
